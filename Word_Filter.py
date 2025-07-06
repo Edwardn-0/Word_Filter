1241ea54caf707f18527e7710c6410a85b70782f
Edwardn-0 <letmegodl@gmail.com>
2025-07-06 17:42:24 +0900

    Add files via upload

--- Word_Filter.py
+++ Word_Filter.py
@@ -1,4 +1,4 @@
-Ban_word = ['마약', '불법', '폭탄', '약품 조제']
+Ban_word = ['drug', 'illegal', 'bomb', 'gambling', 'poison']
 Ban_word = set(Ban_word)
 
 ban_count = 0
@@ -6,10 +6,10 @@ ban_count = 0
 def Word_Filter(input):
     global ban_count
     for words in Ban_word:
-        if words == input:
+        if words in input:
             print(f'{words} 때문에 이 문장은 금지됩니다.')
             ban_count += 1
-        elif words != input:
+        elif words not in input:
             pass
     if ban_count > 0:
         pass
@@ -17,4 +17,6 @@ def Word_Filter(input):
         print('이 문장에는 금지어가 없습니다')
 
 Input_word = (input("문장을 입력하세요: "))
+Input_word = Input_word.replace('?', ' ').replace('!', ' ').replace('.', ' ').replace(',', ' ')
+Input_word = Input_word.split()
 Word_Filter(Input_word)
